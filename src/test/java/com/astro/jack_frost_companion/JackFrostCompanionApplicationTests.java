7b69db5713e2b971a5bd14b94e3f3db989e1ecbc
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-09 20:56:16 -0300

    first commit

---
+++ src/test/java/com/astro/jack_frost_companion/JackFrostCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.jack_frost_companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackFrostCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
